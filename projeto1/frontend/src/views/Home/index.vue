1b0607a5b142e664dc57abcf3455841106f433b6
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-04-25 12:34:56 -0300

    home view

--- projeto1/frontend/src/views/Home/index.vue
+++ projeto1/frontend/src/views/Home/index.vue
@@ -1,10 +1,44 @@
 <template>
-  <h1 class="text-brand-main">Testando</h1>
+  <CustomHeader 
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <Contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker ₢ 2021</p>
+  </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import CustomHeader from './CustomHeader.vue';
+import Contact from './Contact.vue';
 
+export default {
+  components: {CustomHeader, Contact},
+  //função que manipulamos tudo do script no vue3
+  setup(){
+    const router = useRouter();
+    //hook do vue para pegar o token do login
+    onMounted(()=>{
+      const token = window.localStorage.getItem('token');
+      if(token) {
+        router.push({ name:'Feedback' });
+      }
+    })
+    function handleLogin(){
+
+    }
+    function handleAccountCreate(){
+
+    }
+    //temos que criar esse return no vue3
+    return{
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
 
