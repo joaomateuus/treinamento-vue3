8b72986cc3713a8136fa26054e5189d9998fad4d
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-04-26 11:26:12 -0300

    login modal html

--- projeto1/frontend/src/components/ModalLogin/index.vue
+++ projeto1/frontend/src/components/ModalLogin/index.vue
@@ -1,15 +1,76 @@
 <template>
-  <div>
-    <h1 class="">Meu login</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">Entre na sua conta </h1>
+    <button class="text-4xl text-gray-600 focus:outline-none" @click="close">&times;</button>
+  </div>
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+          <input type="email" v-model="state.email.value" :class="{ 'border-brand-danger': !!state.email.errorMessage }"
+          class="block w-full px-4 py-3 mt-1 text-large bg-gray-100 border-2 border-transparent rounded"
+          placeholder="joaomateus@gmail.com">
+        <span
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger">
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <input type="password" v-model="state.password.value" class="block w-full px-4 py-3 mt-1 text-large bg-gray-100 border-2 border-transparent rounded"
+        :class="{
+          'border-brand-danger': !!state.password.errorMessage
+        }"
+        placeholder="****">
+        <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+
+      <button :disabled="state.isLoading"
+      type="submit" :class="{ 'opacity-50': state.isLoading }"
+      class="px-16 py-3 mt-12 ml-20 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150">
+        Entrar
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue';
+import useModal from '../../hooks/useModal'
+
 export default {
-  name:'ModalLogin'
+  name:'ModalLogin',
+  setup() {
+    const modal = useModal();
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: ''
+      },
+      password:{
+        value: '',
+        errorMessage: '',
+      }
+    })
+
+    function handleSubmit(){
+
+    }
+  
+    return {
+      state,
+      close: modal.close
+    }
+  
+  }
+
 }
 </script>
 
-<style>
+<style />
 
-</style>
